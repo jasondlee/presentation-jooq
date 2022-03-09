0a299233cf3235a145f3f289bd3a60a20e72605a
Jason Lee <jason@steeplesoft.com>
2022-03-08 21:16:38 -0600

    Code refactor

--- codegen-demo/src/main/java/com/steeplesoft/jooq/codegen/ChainDemoResource.java
+++ codegen-demo/src/main/java/com/steeplesoft/jooq/codegen/ChainDemoResource.java
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.steeplesoft.jooq.codegen.model.AddressModel;
 import com.steeplesoft.jooq.codegen.model.CityModel;
-import com.steeplesoft.jooq.codegen.model.CountryModel;
 import com.steeplesoft.jooq.codegen.model.StaffModel;
 import com.steeplesoft.jooq.codegen.model.StoreModel;
+import com.steeplesoft.jooq_demo.generated.tables.Address;
+import com.steeplesoft.jooq_demo.generated.tables.City;
+import com.steeplesoft.jooq_demo.generated.tables.Staff;
 import org.jooq.DSLContext;
 import org.jooq.Field;
+import org.jooq.SelectField;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import java.util.List;
 
 import static com.steeplesoft.jooq_demo.generated.tables.Staff.STAFF;
@@ -38,32 +40,8 @@ public class ChainDemoResource {
     public List<StoreModel> chainedStoresAndStaff() throws JsonProcessingException {
         List<StoreModel> stores = dsl.select(
                         STORE.STORE_ID,
-                        row(
-                                STORE.address().ADDRESS_ID,
-                                STORE.address().ADDRESS_,
-                                STORE.address().ADDRESS2,
-                                STORE.address().DISTRICT,
-                                row(
-                                        STORE.address().city().CITY_ID,
-                                        STORE.address().city().CITY_,
-                                        STORE.address().city().LAST_UPDATE
-                                ).mapping(CityModel::new).as("city"),
-                                STORE.address().POSTAL_CODE,
-                                STORE.address().PHONE,
-                                STORE.address().LAST_UPDATE
-                        ).mapping(AddressModel::new).as("address"),
-                        multiset(
-                                select(
-                                        STORE.staff().STAFF_ID,
-                                        STORE.staff().FIRST_NAME,
-                                        STORE.staff().LAST_NAME,
-                                        STORE.staff().USERNAME,
-                                        STORE.staff().EMAIL,
-                                        STORE.staff().LAST_UPDATE
-                                )
-                                        .from(STAFF)
-                                        .where(STORE.staff().STORE_ID.eq(STORE.STORE_ID))
-                        ).as("staff").convertFrom(r -> r.map(mapping(StaffModel::new)))
+                        addressRow(STORE.address()),
+                        staffMultiset(STORE.staff())
                 )
                 .from(STORE)
                 .fetch()
@@ -72,5 +50,41 @@ public class ChainDemoResource {
                         (List<StaffModel>) r.get("staff")));
         System.out.println(mapper.writeValueAsString(stores));
         return stores;
+    }
+
+    private SelectField<List<StaffModel>> staffMultiset(Staff staff) {
+        return multiset(
+                select(
+                        staff.STAFF_ID,
+                        staff.FIRST_NAME,
+                        staff.LAST_NAME,
+                        staff.USERNAME,
+                        staff.EMAIL,
+                        staff.LAST_UPDATE
+                )
+                        .from(STAFF)
+                        .where(staff.STORE_ID.eq(STORE.STORE_ID))
+        ).as("staff").convertFrom(r -> r.map(mapping(StaffModel::new)));
+    }
+
+    private SelectField<AddressModel> addressRow(Address address) {
+        return row(
+                address.ADDRESS_ID,
+                address.ADDRESS_,
+                address.ADDRESS2,
+                address.DISTRICT,
+                cityRow(address.city()),
+                address.POSTAL_CODE,
+                address.PHONE,
+                address.LAST_UPDATE
+        ).mapping(AddressModel::new).as("address");
+    }
+
+    private SelectField<CityModel> cityRow(City city) {
+        return row(
+                city.CITY_ID,
+                city.CITY_,
+                city.LAST_UPDATE
+        ).mapping(CityModel::new).as("city");
     }
 }
